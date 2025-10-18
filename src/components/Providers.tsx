2c8a190add9baf753e0d8f142b21a76a4bc963e7
Kevin Blair <kblair40@gmail.com>
2025-10-18 13:45:34 -0500

    Cleanup boilerplate

---
+++ src/components/Providers.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const Providers = ({ children }: React.PropsWithChildren) => {
+  return <div className="border">{children}</div>;
+};
+
+export default Providers;
