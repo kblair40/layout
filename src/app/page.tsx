e81e8885721f2e496ca54e77322909bc6fa9258a
Kevin Blair <kblair40@gmail.com>
2025-11-08 14:45:37 -0500

    Replace FloorPlan with Stage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import dynamic from "next/dynamic";
 const FloorPlan = dynamic(() => import("@/components/FloorPlan/FloorPlan"), {
   ssr: false,
 });
+const Stage = dynamic(() => import("@/components/Stage/Stage"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <div className="font-sans min-h-screen">
-      {/* <Stage /> */}
-      <FloorPlan />
+      <Stage />
+      {/* <FloorPlan /> */}
     </div>
   );
 }
