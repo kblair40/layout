e1f0732956f5e655210f3ac2402107c65a28a19e
Kevin Blair <kblair40@gmail.com>
2025-11-09 13:41:14 -0500

    Move all listeners to hook

--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -10,6 +10,7 @@ import { Line as LineNode } from "konva/lib/shapes/Line";
 import Konva from "konva";
 import type { LineConfig } from "konva/lib/shapes/Line";
 import type { Vector2d } from "konva/lib/types";
+import useEventListeners from "@/hooks/useEventListeners";
 
 type KonvaMouseEvent = Konva.KonvaEventObject<MouseEvent>;
 
@@ -20,145 +21,60 @@ const DEFAULT_LINE: Partial<LineConfig> = {
 };
 
 const Stage = () => {
-  const [lines, setLines] = useState<LineConfig[]>([]);
-  const [lineNodes, setLineNodes] = useState<LineNode[]>([]);
-
   const isDrawing = useRef(false);
-  // const layerRef = useRef<KonvaNodeComponent<LayerType, LayerConfig> | null>(
   const layerRef = useRef<LayerType>(null);
-
-  function getMousePosition(e: KonvaMouseEvent): Vector2d {
-    const pos = e.target.getStage()?.getPointerPosition();
-    if (!pos) throw new Error("STAGE NOT PRESENT");
-
-    return pos;
-  }
-
-  function getLastLine() {
-    const lastLine = lines[lines.length - 1];
-
-    if (!lastLine) {
-      throw new Error("No line found");
-    }
-
-    return lastLine;
-  }
-
-  const handleMouseDown = (e: KonvaMouseEvent) => {
-    console.log("\nMOUSE DOWN");
-    isDrawing.current = true;
-    const pos = getMousePosition(e);
-    setLines([...lines, { points: [pos.x, pos.y] }]);
-  };
-
-  const handleMouseMove = (e: KonvaMouseEvent) => {
-    // no drawing - skipping
-    if (!isDrawing.current) return;
-
-    const point = getMousePosition(e);
-    const lastLine = getLastLine();
-
-    // add point
-    lastLine.points = (lastLine.points as number[])
-      .slice(0, 2)
-      .concat([point.x, point.y]);
-
-    lines.splice(lines.length - 1, 1, lastLine);
-    setLines(lines.slice());
-  };
-
-  const handleMouseUp = (e: KonvaMouseEvent) => {
-    console.log("MOUSE UP");
-    isDrawing.current = false;
-
-    const stage = e.target.getStage();
-    if (!stage) {
-      console.log("NO STAGE");
-      return;
-    }
-
-    const layers = stage.getLayers();
-    console.log("LAYERS:", layers);
-
-    const point = getMousePosition(e);
-    const lastLine = getLastLine();
-    console.log("MOUSEUP POINT:", point);
-
-    // add point
-    lastLine.points = (lastLine.points as number[])
-      .slice(0, 2)
-      .concat([point.x, point.y]);
-
-    lines.splice(lines.length - 1, 1, lastLine);
-
-    setLines(lines);
-    console.log("SETTING LINES TO:", [...lines], "\n\n");
-  };
-
-  const handleDragStart = (e: KonvaMouseEvent) => {
-    const id = e.target.id();
-    console.log("Drag Start:", id);
-    e.evt.preventDefault();
-    setLines(
-      lines.map((line) => {
-        console.log("LINE:", line);
-        return {
-          ...line,
-          isDragging: line.id === id,
-        };
-      })
-    );
-  };
-
-  function handleMouseEnter(e: KonvaMouseEvent) {
-    console.log("E.TARGET:", {
-      target: e.target,
-      currentTarget: e.currentTarget,
-    });
-  }
-  function handleClickLine(e: KonvaMouseEvent) {
-    console.log("CLICK");
-    console.log("E.TARGET:", {
-      target: e.target,
-      currentTarget: e.currentTarget,
-    });
-  }
-
-  //   if (typeof window === "undefined") {
-  //     return null;
-  //   }
 
   const canvas = useRef<Konva.Stage>(null);
 
-  return (
-    <Canvas
-      ref={canvas}
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseup={handleMouseUp}
-      width={window?.innerWidth || 0}
-      height={window?.innerHeight || 0}
-    >
-      <Layer ref={layerRef}>
-        {lines.map((line, i) => {
-          return (
-            <Line
-              {...DEFAULT_LINE}
-              {...line}
-              onDragStart={handleDragStart}
-              key={i}
-              onMouseOver={handleMouseEnter}
-              draggable={true}
-              onClick={handleClickLine}
-            />
-          );
-        })}
+  const { listeners, lines, stageListenersActive, setStageListenersActive } =
+    useEventListeners({ refs: { stage: canvas } });
 
-        {/* {lineNodes.map((node, i) => {
+  return (
+    <div>
+      <Canvas
+        ref={canvas}
+        className="border"
+        onMouseDown={
+          stageListenersActive ? listeners.handleMouseDownOnStage : undefined
+        }
+        onMouseMove={
+          stageListenersActive ? listeners.handleMouseMoveOnStage : undefined
+        }
+        onMouseup={
+          stageListenersActive ? listeners.handleMouseUpOnStage : undefined
+        }
+        width={window?.innerWidth || 0}
+        height={window?.innerHeight - 100 || 0}
+      >
+        <Layer ref={layerRef}>
+          {lines.map((line, i) => {
+            return (
+              <Line
+                {...DEFAULT_LINE}
+                {...line}
+                onDragStart={listeners.handleDragStartLine}
+                key={i}
+                draggable={true}
+                onClick={listeners.handleClickLine}
+              />
+            );
+          })}
+
+          {/* {lineNodes.map((node, i) => {
           return node.
         })} */}
-      </Layer>
-    </Canvas>
+        </Layer>
+      </Canvas>
+
+      <div className="h-[100px] flex items-center gap-x-4 px-4">
+        <button
+          className="border px-2 py-1 flex justify-center items-center rounded-sm bg-white transition-colors hover:bg-neutral-100 duration-150 cursor-pointer active:bg-neutral-200"
+          onClick={() => setStageListenersActive((cur) => !cur)}
+        >
+          {stageListenersActive ? "disable" : "enable"} stage listeners
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/hooks/useEventListeners.ts
+++ src/hooks/useEventListeners.ts
@@ -1,5 +1,7 @@
 "use client";
-import type { RefObject } from "react";
+import { useRef, useState, useEffect, type RefObject } from "react";
+import type { LineConfig } from "konva/lib/shapes/Line";
+import type { Vector2d } from "konva/lib/types";
 import Konva from "konva";
 
 type KonvaMouseEvent = Konva.KonvaEventObject<MouseEvent>;
@@ -8,6 +10,128 @@ type Refs = {
   stage: RefObject<Konva.Stage | null>;
 };
 
-function useEventListeners({ refs }: { refs: Refs }) {}
+function useEventListeners({ refs }: { refs: Refs }) {
+  const [stageListenersActive, setStageListenersActive] = useState(true);
+  const [lines, setLines] = useState<LineConfig[]>([]);
+
+  const isDrawing = useRef(false);
+
+  //   }, [refs]);
+
+  function getMousePosition(e: KonvaMouseEvent): Vector2d {
+    const pos = e.target.getStage()?.getPointerPosition();
+    if (!pos) throw new Error("STAGE NOT PRESENT");
+
+    return pos;
+  }
+
+  function getLastLine() {
+    const lastLine = lines[lines.length - 1];
+
+    if (!lastLine) {
+      throw new Error("No line found");
+    }
+
+    return lastLine;
+  }
+
+  const handleMouseDown = (e: KonvaMouseEvent) => {
+    console.log("\nMOUSE DOWN");
+    isDrawing.current = true;
+    const pos = getMousePosition(e);
+    setLines([...lines, { points: [pos.x, pos.y] }]);
+  };
+
+  const handleMouseMove = (e: KonvaMouseEvent) => {
+    // no drawing - skipping
+    if (!isDrawing.current) return;
+
+    const point = getMousePosition(e);
+    const lastLine = getLastLine();
+
+    // add point
+    lastLine.points = (lastLine.points as number[])
+      .slice(0, 2)
+      .concat([point.x, point.y]);
+
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.slice());
+  };
+
+  const handleMouseUp = (e: KonvaMouseEvent) => {
+    console.log("MOUSE UP");
+    isDrawing.current = false;
+
+    const stage = e.target.getStage();
+    if (!stage) {
+      console.log("NO STAGE");
+      return;
+    }
+
+    const layers = stage.getLayers();
+    console.log("LAYERS:", layers);
+
+    const point = getMousePosition(e);
+    const lastLine = getLastLine();
+    console.log("MOUSEUP POINT:", point);
+
+    // add point
+    lastLine.points = (lastLine.points as number[])
+      .slice(0, 2)
+      .concat([point.x, point.y]);
+
+    lines.splice(lines.length - 1, 1, lastLine);
+
+    setLines(lines);
+    console.log("SETTING LINES TO:", [...lines], "\n\n");
+  };
+
+  useEffect(() => {
+    console.log("Stage change:", refs.stage.current);
+    const stage = refs.stage.current;
+    // if (stage) {
+    //   stage.addEventListener("mousedown", handleMouseDown);
+    //   stage.add;
+    // }
+  }, []);
+
+  const handleDragStart = (e: KonvaMouseEvent) => {
+    const id = e.target.id();
+    console.log("Drag Start:", id);
+    e.evt.preventDefault();
+    setLines(
+      lines.map((line) => {
+        console.log("LINE:", line);
+        return {
+          ...line,
+          isDragging: line.id === id,
+        };
+      })
+    );
+  };
+
+  function handleClickLine(e: KonvaMouseEvent) {
+    console.log("CLICK");
+    console.log("E.TARGET:", {
+      target: e.target,
+      currentTarget: e.currentTarget,
+    });
+  }
+
+  const listeners = {
+    handleMouseDownOnStage: handleMouseDown,
+    handleMouseUpOnStage: handleMouseUp,
+    handleMouseMoveOnStage: handleMouseMove,
+    handleDragStartLine: handleDragStart,
+    handleClickLine,
+  };
+
+  return {
+    listeners,
+    lines,
+    stageListenersActive,
+    setStageListenersActive,
+  };
+}
 
 export default useEventListeners;
